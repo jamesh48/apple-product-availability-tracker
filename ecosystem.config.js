ec0225faaa45076bb4ad9fca505a8ed4ec2cff1f
James Hrivnak <jameshrivnak@Jamess-Mac-Studio.local>
2022-06-05 08:05:01 -0600

    initial pm2 stup

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{ name: "apple-product-tracker", script: "./server/dist/index.js" }],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-3-14-206-30.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/tutorial-2.pem",
+      ref: "origin/main",
+      repo: "https://github.com/jamesh48/apple-product-availability-tracker.git",
+      path: "/home/ubuntu/apple-product-availability-tracker",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js",
+    },
+  },
+};
